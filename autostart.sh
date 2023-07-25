edb24eba42bfcd03bd3bbd309dc6dd046cc90644
folospior <111700958+folospior@users.noreply.github.com>
2023-07-25 20:07:00 +0200

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,8 @@
+slstatus &
+picom &
+nm-applet &
+feh --bg-scale ~/Photos/dracula-cat-282a36.png
+xfce4-power-manager &
+xmodmap -e "clear lock"
+xmodmap -e "keycode 39 ="
+xmodmap -e "keycode 66 = s"
