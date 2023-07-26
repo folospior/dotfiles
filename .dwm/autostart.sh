7a0c91b395b743f30b2c6ee79848f3158b666693
folospior <111700958+folospior@users.noreply.github.com>
2023-07-26 11:40:25 +0200

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,8 @@
 slstatus &
-xmodmap -e "clear lock"
-xmodmap -e "keycode 66 = s"
-xmodmap -e "keycode 39 ="
-nitrogen --restore
 picom &
+nm-applet &
+feh --bg-scale ~/Photos/dracula-cat-282a36.png
+xfce4-power-manager &
+xmodmap -e "clear lock"
+xmodmap -e "keycode 39 ="
+xmodmap -e "keycode 66 = s"
