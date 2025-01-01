9df8636cf7a6a0d514333aff899e8fa89ff4f85a
Filip Hoffmann <folosp2@gmail.com>
2025-01-01 20:50:04 +0100

    add hyprpaper.... stuff

--- user/hyprland.nix
+++ user/hyprland.nix
@@ -1,42 +1,52 @@
 {pkgs, ...}: {
   home.packages = [pkgs.alacritty pkgs.rofi-wayland]; # temporary
+
+  services.hyprpaper.enable = true;
+
   wayland.windowManager.hyprland = {
     enable = true;
-  };
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      "eDP-1, 1920x1080@60, 0x0, 1"
-      "HDMI-A-1, 1920x1080@60, 1920x0, 1"
-    ];
+    systemd.enable = false;
+    settings = {
 
-    "$mod" = "SUPER";
+      exec-once = [
+        "$HOME/pick_random_wallpapers.sh"
+      ];
 
-    bind =
-      [
-        "$mod, B, exec, firefox"
-        "$mod SHIFT, Return, exec, alacritty"
-        "$mod, V, exec, code"
-        "$mod SHIFT, Q, killactive"
-        "$mod SHIFT, E, exit"
-        "$mod, P, exec, rofi -show drun"
-      ]
-      ++ ( # i just took this from the hyprland wiki and it works so i guess??
-        builtins.concatLists (builtins.genList (
-            i: let
-              ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-          9)
-      );
+      monitor = [
+        "eDP-1, 1920x1080@60, 0x0, 1"
+        "HDMI-A-1, 1920x1080@60, 1920x0, 1"
+      ];
 
-    bindm = [
-      # mouse movements
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-      "$mod ALT, mouse:272, resizewindow"
-    ];
+      "$mod" = "SUPER";
+
+      bind =
+        [
+          "$mod, B, exec, firefox"
+          "$mod SHIFT, Return, exec, alacritty"
+          "$mod, V, exec, code"
+          "$mod SHIFT, Q, killactive"
+          "$mod SHIFT, E, exit"
+          "$mod, P, exec, rofi -show drun"
+          "$mod, O, exec, $HOME/pick_random_wallpaper.sh"
+        ]
+        ++ ( # i just took this from the hyprland wiki and it works so i guess??
+          builtins.concatLists (builtins.genList (
+              i: let
+                ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            9)
+        );
+
+      bindm = [
+        # mouse movements
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod ALT, mouse:272, resizewindow"
+      ];
+    };
   };
 }
