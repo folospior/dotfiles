6a2561e5ff1088f71aa471b5711dc9191296aea2
Filip Hoffmann <folosp2@gmail.com>
2025-01-02 11:04:31 +0100

    some qt theming

--- user/gtk.nix
+++ user/gtk.nix
@@ -12,6 +12,14 @@
     };
   };
 
+  qt = {
+    enable = true;
+    platformTheme.name = "qt5ct";
+    style.name = "Utterly Nord Plasma";
+    style.package = pkgs.utterly-nord-plasma;
+  };
+  home.packages = with pkgs; [libsForQt5.qtstyleplugin-kvantum libsForQt5.qt5ct];
+
   home.pointerCursor = {
     package = pkgs.posy-cursors;
     name = "Posy_Cursor_Black"; # has to be file name
