e74348f2fa4b1b004d4f0f1a2ef8f739b8a42469
Filip Hoffmann <folosp2@gmail.com>
2025-01-02 12:17:36 +0100

    change git commit editor

--- user/git.nix
+++ user/git.nix
@@ -3,6 +3,9 @@
     enable = true;
     userName = userSettings.fullName;
     userEmail = userSettings.email;
+    extraConfig = {
+    	core.editor = "nvim";
+    };
   };
   programs.gh.enable = true;
 }
