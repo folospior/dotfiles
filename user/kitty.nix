4b40be27862259af97f8fd561f9249270e1a593a
Filip Hoffmann <folosp2@gmail.com>
2025-01-11 13:36:48 +0100

    kitty wowowowowo

--- user/kitty.nix
+++ user/kitty.nix
@@ -10,9 +10,15 @@
       scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
-      font_size = 15.0;
+      font_size = 14.5;
+      cursor_shape = "block";
       cursor_blink_interval = "cubic-bezier(0.7, 0, 0.84, 0)";
       cursor_trail = 1;
+      background_opacity = 0.8; 
+    };
+    shellIntegration = {
+      mode = "no-cursor";
+      enableBashIntegration = true;
     };
   };
 }
