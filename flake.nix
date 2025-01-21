9e33410afd98a4ad42aaab071077752a728f4617
Filip Hoffmann <folosp2@gmail.com>
2025-01-21 10:20:07 +0100

    remove nixvim following update to nvf

--- flake.nix
+++ flake.nix
@@ -8,10 +8,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
