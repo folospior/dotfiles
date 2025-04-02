a3f118e732c12a22232c451abe32c5717c9806b8
Filip Hoffmann <folosp@protonmail.com>
2025-04-02 20:04:39 +0200

    switch to sober flathub version

--- system/flatpak.nix
+++ system/flatpak.nix
@@ -3,10 +3,7 @@
     enable = true;
     update.onActivation = true;
     packages = [
-      {
-        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
-        sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
-      }
+      "org.vinegarhq.Sober"
       "com.obsproject.Studio" # only the flatpak is the supported way of installing obs studio
       "com.usebottles.bottles"
     ];
@@ -15,7 +12,7 @@
         # do not touch, makes sober run on the iGPU and not crash lol
         VK_DRIVER_FILES = "/usr/lib/x86_64-linux-gnu/GL/vulkan/icd.d/intel_icd.x86_64.json";
       };
-      "com.usebottles.bottles".Context.filesystems = [ "home" ];
+      "com.usebottles.bottles".Context.filesystems = ["home"];
     };
   };
 }
