029ebbd1984dcee30000d09ec0e5e025c0e27c66
Filip Hoffmann <folosp@protonmail.com>
2025-05-01 18:53:34 +0200

    Revert "font switch AGAIN lmfao" (back to IntoneMono)
    This reverts commit 1b4148434cf93a37e59b23e1bf5bd089aff5a4c4.

--- user/kitty.nix
+++ user/kitty.nix
@@ -3,8 +3,8 @@
     enable = true;
     themeFile = "Nord";
     font = {
-      name = "JetBrainsMono Nerd Font";
-      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "IntoneMono Nerd Font";
+      package = pkgs.nerd-fonts.intone-mono;
     };
     settings = {
       # TODO: Add on-click events
@@ -12,6 +12,7 @@
       enable_audio_bell = false;
       update_check_interval = 0;
       font_size = 13.5;
+      font_features = "+ss01";
       cursor_shape = "block";
       cursor_blink_interval = "cubic-bezier(0.7, 0, 0.84, 0)";
       cursor_trail = 1;
