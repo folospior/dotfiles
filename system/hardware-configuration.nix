a0442bd7d750cbe0556d62ec55ac093fda8759a1
Filip Hoffmann <folosp2@gmail.com>
2025-06-19 12:08:08 +0000

    no filesystems in hardware-configurationcp /mnt/etc/nixos/hardware-configuration.nix system/hardware-configuration.nix

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -13,19 +13,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a3fa4ee9-36cb-4ee3-a4ef-4191b07952ba";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/AEEC-4BEB";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
