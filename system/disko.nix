01d2cecc58337fee5145cf8149eadb0ccd5ea5f1
Filip Hoffmann <folosp2@gmail.com>
2025-06-19 12:09:27 +0000

    me when no git add .

---
+++ system/disko.nix
@@ -0,0 +1,36 @@
+{...}: {
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = ["umask=0077"];
+          };
+        };
+        swap = {
+          size = "10G";
+          content = {
+            type = "swap";
+            discardPolicy = "both";
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
+}
